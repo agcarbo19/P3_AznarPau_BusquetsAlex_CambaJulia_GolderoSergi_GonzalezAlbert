f318579f631a68c636579eb2fe9e63b4fb17332b
juliaCamba <79279391+juliaCamba@users.noreply.github.com>
2023-03-19 22:14:04 +0100

    algo extra

--- PA_BOB_W23_STU/Assets/BOBsInLove/BT_Daisy.cs
+++ PA_BOB_W23_STU/Assets/BOBsInLove/BT_Daisy.cs
@@ -4,31 +4,35 @@ using BTs;
 [CreateAssetMenu(fileName = "BT_Daisy", menuName = "Behaviour Trees/BT_Daisy", order = 1)]
 public class BT_Daisy : BehaviourTree
 {
-   
 
-     // construtor
-    public BT_Daisy()  { 
+
+    // construtor
+    public BT_Daisy()
+    {
         /* Receive BT parameters and set them. Remember all are of type string */
     }
-    
+
     public override void OnConstruction()
     {
         DAISY_Blackboard bbd = (DAISY_Blackboard)blackboard;
 
         RepeatForeverDecorator pace = new RepeatForeverDecorator(
-            new Sequence (
-                new LambdaAction(() => {
+            new Sequence(
+                new LambdaAction(() =>
+                {
                     GameObject corner = bbd.GetRandomCorner();
                     bbd.Put("theCorner", corner);
-                    return Status.SUCCEEDED; }
+                    return Status.SUCCEEDED;
+                }
                 ),
                 new ACTION_Arrive("theCorner")
-            )    
+            )
         );
 
         RepeatForeverDecorator mumble = new RepeatForeverDecorator(
             new Sequence(
-                new LambdaAction(() => {
+                new LambdaAction(() =>
+                {
                     string utterance = bbd.GetRandomUtterance();
                     bbd.Put("theUtterance", utterance);
                     return Status.SUCCEEDED;
@@ -36,7 +40,7 @@ public class BT_Daisy : BehaviourTree
                 ),
                 new ACTION_Speak("theUtterance"),
                 new ACTION_WaitForSeconds("5")
-                //new ACTION_Quiet()
+            //new ACTION_Quiet()
             )
         );
 
@@ -47,21 +51,51 @@ public class BT_Daisy : BehaviourTree
 
 
         DynamicSelector dyn = new DynamicSelector();
+
+     
+
+
         dyn.AddChild(
-            new CONDITION_InstanceNear("15", "BOUQUET", "true"),
-            new Sequence(
-                new ACTION_Speak("Finally here!"),
-                new ACTION_WaitForSeconds("7"),
-                new ACTION_Quiet(),
-                new LambdaAction(() => { bbd.CloseDoor(); return Status.SUCCEEDED; }),
-                new ACTION_Activate("hearts"),
-                new ACTION_RunForever()
-            )
-        ) ;
+           new CONDITION_InstanceNear("15", "BOUQUET", "true"),
+
+           new Sequence(
+               new ACTION_Speak("Finally here!"),
+               new ACTION_WaitForSeconds("2"),
+               new ACTION_Quiet(),
+               new LambdaAction(() => { bbd.CloseDoor(); return Status.SUCCEEDED; }),
+               new ACTION_Activate("hearts"),
+               new ACTION_RunForever(),
+
+               new Selector(
+                   new CONDITION_InstanceNear("15", "RING", "true"),
+                   new Sequence( 
+                    new ACTION_WaitForSeconds("7"),
+                    new ACTION_Quiet(),
+                    new ACTION_Activate("hearts"),
+                    new ACTION_Activate("HappyEnd"),
+                    new ACTION_RunForever()
+                    ),
+
+                   new CONDITION_InstanceNear("15", "NECKLACE", "true"),
+                   new Sequence(
+                    new ACTION_Speak("ABSOLUTELY NOT!"),
+                    new ACTION_WaitForSeconds("7"),
+                    new ACTION_Quiet(),
+                    new LambdaAction(() => { bbd.CloseDoor(); return Status.SUCCEEDED; }),
+                    new ACTION_Activate("MIDDLE_FINGER"),
+                    new ACTION_RunForever()
+           )
+
+
+               )
+
+       ));
+
         dyn.AddChild(
-            new CONDITION_AlwaysTrue(),
-            paceAndMumble
-        );
+         new CONDITION_AlwaysTrue(),
+         paceAndMumble
+     );
+
 
         root = dyn;
     }
